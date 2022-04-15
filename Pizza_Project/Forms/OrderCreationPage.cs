334285d9f0a92c4522bf3bdbe7d378f366b21268
Javani Wright <javaniwright@gmail.com>
2022-04-14 20:11:28 -0400

    Finished layout of order creation class

--- Pizza_Project/Forms/OrderCreationPage.cs
+++ Pizza_Project/Forms/OrderCreationPage.cs
@@ -16,5 +16,15 @@ namespace Pizza_Project.Forms
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
